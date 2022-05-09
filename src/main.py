b5bd3ec039e4b111281b135ebd9b7eaca4eb55aa
Hippolyte <hippolyte.rm@hotmail.fr>
2022-05-10 00:45:32 +0100

    Added isort

--- src/main.py
+++ src/main.py
@@ -1,13 +1,16 @@
 from logzero import logger
+import os
+
 
 
 def main_func(dummy=314):
     """Just a dumnb function."""
     print("it ran main.")
     test_for_long_line = dummy + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1
+    print(f"PYTHONPATH: {os.getenv('PYTHONPATH')}")
     return dummy
 
 
 if __name__ == "__main__":
-    logger.info("TESTTTTTTTTTTT")
+    logger.info(        "TESTTTTTTTTTTT")
     main_func()
